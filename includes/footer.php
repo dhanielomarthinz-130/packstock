da745dcc928a7099dbb0af1fb71a244d555390a3
dhanielomarthinz-130 <dhanielomarthinz@gmail.com>
2026-08-29 11:06:21 +0700

    fix: add cache-buster to app.js to prevent stale browser cache

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,9 @@
 <?php
 // includes/footer.php - Global Scripts & Footer
 $baseUrl = Auth::getBaseUrl();
+$cacheBuster = time();
 ?>
   <!-- Shared Global JS -->
-  <script src="<?= $baseUrl ?>/assets/js/app.js"></script>
+  <script src="<?= $baseUrl ?>/assets/js/app.js?v=<?= $cacheBuster ?>"></script>
 </body>
 </html>
